d7244cfcfbc8f6a83fbec28e66b0542af503c2c5
AeonLucid <aeonlucid@gmail.com>
2022-02-01 20:38:19 +0100

    Aggressive inlining

--- src/UnicornNet/Utils/MemoryUtils.cs
+++ src/UnicornNet/Utils/MemoryUtils.cs
@@ -1,4 +1,5 @@
 using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
 
 namespace UnicornNet.Utils
 {
@@ -6,16 +7,19 @@ namespace UnicornNet.Utils
     {
         public const ulong UnicornPageSize = 0x1000;
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong AlignPageUp(ulong size)
         {
             return (size + UnicornPageSize - 1) & ~(UnicornPageSize - 1);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong AlignPageDown(ulong size)
         {
             return size & ~(UnicornPageSize - 1);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static byte[] ToBytes(ulong value)
         {
             var bytes = new byte[8];
